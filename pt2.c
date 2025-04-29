8e51d42216ab44a458fe86bfb0f6f2392cc98f30
12402111 <138605316+12402111@users.noreply.github.com>
2025-04-29 18:10:58 +0600

    Update pt2.c

--- pt2.c
+++ pt2.c
@@ -1,38 +1,21 @@
 #include<stdio.h>
 int main()
-
-
 {
-    int n;
-
-
-
-
-    int max =0;
-    int sm =0;
+    int n;int max =-1; int sm =-1;
     scanf("%d", &n);
-
-
     int r1=n%10;
     n=n/10 ;
-
-
     if(r1>max)
     {
         max =r1;
-sm = r1;
+
     }
     int r2 = n%10;
     n=n/10;
-
-
     if(r2 > max)
     {
         sm = max;
         max = r2;
-    } else if (r2 == max)
-    {
-        // ignore
     }
     else if (r2 > sm)
     {
@@ -46,15 +29,11 @@ sm = r1;
     {
         sm = max;
         max = r3;
-    } else if (r3 == max)
-    {
-        // ignore
     }
     else if (r3 > sm)
     {
         sm = r3;
     }
-
     int r4 = n%10;
     n=n/10;
 
@@ -63,27 +42,18 @@ if(r4 > max)
         sm = max;
         max = r4;
     }
-     else if (r4 == max)
-    {
-        // ignore
-    }
     else if (r4 > sm)
     {
         sm = r4;
     }
-if(max!=sm)
+if(sm!=max)
    {
-
-   printf("m:%d s:%d", max, sm);}
+   printf("m:%d s:%d", max, sm);
+   }
 
   else{
-
-       int x =-1;
-   printf("%d",x);
+   printf("-1");
 }
-
+main();
     return 0;
-
-
-
 }
